a93444fc81795c78efe5fa6583e2fc81eb0d1d8b
EgorBray <revineg2002@gmail.com>
2024-08-04 23:51:56 +1000

    Add java project

---
+++ src/main/java/org/example/controller/dto/UserResponse.java
@@ -0,0 +1,14 @@
+package org.example.controller.dto;
+
+import java.time.LocalDateTime;
+
+public class UserResponse {
+    public int id;
+
+    public String login;
+    public String name;
+    public String lastName;
+    public double amount;
+
+    public LocalDateTime creationDate;
+}
